1ac0a44c6c78793718d1f7de150d029fedd7ef7f
jsimonferrer <coquillo70@hotmail.com>
2024-01-11 18:50:45 +0100

    Init proyect

---
+++ src/test/java/com/manage/guardias/GuardiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manage.guardias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
